344609ae8daa902041ac469e690871f353123601
jv199768 <154536624+jv199768@users.noreply.github.com>
2024-05-25 07:32:33 -0400

    Create bite_210.py

---
+++ bite_210.py
@@ -0,0 +1,13 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+from typing import List
+def sum_numbers(numbers: List[int]) -> int:
+"""[Summary]
+
+:param [numbers]: A list of integers, defaults to None
+:type [numbers]: integer, None
+...
+:return: int (the sum of all numbers between 1 and 100  if type is None, or the sum of your list)
+:rtype: int
+"""
+pass
