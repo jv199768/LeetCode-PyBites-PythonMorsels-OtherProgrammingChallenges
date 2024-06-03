a652ac1ddcfdc24e9c751fc13dd71a2da7f9d2eb
Jaivir Baweja <154536624+jv199768@users.noreply.github.com>
2024-06-03 07:20:02 -0400

    Create bite_251.py

---
+++ bite_251.py
@@ -0,0 +1,25 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+import pandas as pd
+from string import ascii_lowercase, ascii_uppercase
+
+def basic_series():
+    return pd.Series(data = [1,2,3,4,5], name = 'Fred')
+
+def float_series():
+    start = 0.000
+    end = 0.000
+    step = 0.001
+    arr = []
+    while start <= end:
+        start = start + step
+        arr.append(start)
+        start = round(start, 3)
+    return pd.Series(data=arr)
+
+def alpha_index_series():
+    return pd.Series(data=list(range(1,27)), index = list(ascii_lowercase))
+
+def object_values_series():
+    return pd.Series(data=list(ascii_uppercase), index = list(range(101,127)))
