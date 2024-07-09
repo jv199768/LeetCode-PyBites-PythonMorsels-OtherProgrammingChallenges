501548e03b0b72d48064e728758fe98a0401ce63
Jaivir Baweja <154536624+jv199768@users.noreply.github.com>
2024-07-09 08:17:49 -0400

    Create divide_two_integers.py

---
+++ divide_two_integers.py
@@ -0,0 +1,36 @@
+class Solution:
+    def divide(self, dividend, divisor):
+        """
+        :type dividend: int
+        :type divisor: int
+        :rtype: int
+        """
+        if (divisor == 1) or (divisor== -1):
+            dividend = divisor * dividend
+            if dividend > 2**31 - 1:
+                return 2**31 - 1
+
+            if dividend < -2**31:
+                return -2**31
+
+            return dividend
+
+        quotient = 0
+        sign = 1
+        if dividend * divisor < 0:
+            sign = -1
+
+        if dividend < 0: dividend = -dividend
+        if divisor < 0: divisor = -divisor
+
+        i = 1
+        sum_all = 0
+        while sum_all <= dividend:
+            if dividend - sum_all < divisor * i:
+                i = 1
+            quotient += i
+            sum_all += divisor * i
+            i += 1
+
+        return (quotient - 1) * sign
+        
