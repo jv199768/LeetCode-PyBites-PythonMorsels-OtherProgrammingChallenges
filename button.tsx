44b4a1b9d91b1ba2c2816456a56f905b2f8541ad
Jaivir Baweja <154536624+jv199768@users.noreply.github.com>
2024-07-12 15:37:42 -0400

    Add files via upload

---
+++ button.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+export default function Button() {
+    const [count, setCount] = useState(0);
+
+    function handleClick() {
+        setCount(count + 1);
+    }
+    return (
+        <button
+            onClick={handleClick}
+            className="bg-blue-500 hover:bg-blue-700 rounded text-white font-bold px-4 py-2"
+        >
+            I have been clicked {count} times
+        </button>
+    );
+}
---
+++ page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Button from "@/components/button";
+
+export default function Home() {
+    return (
+        <main className="flex min-h-screen flex-col items-center p-24 gap-4">
+            <h1>React Counter Button</h1>
+            <Button />
+            <Button />
+        </main>
+    );
+}
