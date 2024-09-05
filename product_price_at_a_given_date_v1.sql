aa828b345f018b72e585c524a8888c10a95c56e1
Jaivir Baweja <154536624+jv199768@users.noreply.github.com>
2024-09-05 16:44:20 -0400

    Create product_price_at_a_given_date_v1.sql

---
+++ product_price_at_a_given_date_v1.sql
@@ -0,0 +1,19 @@
+SELECT product_id
+        , 10 AS price
+FROM products
+GROUP BY product_id
+HAVING MIN(change_date) >= '2019-08-16' AND MIN(price) >- 10
+
+UNION
+
+SELECT product_id
+        , new_price as price
+FROM (
+    SELECT *
+            , ROW_NUMBER() OVER (PARTITION BY product_id ORDER BY change_date DESC) as new_date
+    FROM products
+    WHERE change_date <= '2019-08-16'
+) as t
+WHERE new_date = 1
+
+ORDER BY product_id ASC;
