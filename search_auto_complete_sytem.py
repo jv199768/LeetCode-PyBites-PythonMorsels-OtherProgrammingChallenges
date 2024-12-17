4177cc3e80bedf337c54fd24ab5f6a9369fb1d1f
Jaivir Baweja <154536624+jv199768@users.noreply.github.com>
2024-12-17 09:49:35 -0500

    Add files via upload
    ['iroman', 'i love leetcode', 'i love you'] [] [] [] ['i love you', 'island', 'i love leetcode'] ['i love you', 'island', 'i love leetcode'] [] []

---
+++ search_auto_complete_sytem.py
@@ -0,0 +1,70 @@
+from typing import List
+# Online Python - IDE, Editor, Compiler, Interpreter
+class Trie:
+    def __init__(self):
+        self.children = [None] * 27
+        self.v = 0
+        self.w = ''
+
+    def insert(self, w, t):
+        node = self
+        for c in w:
+            idx = 26 if c == ' ' else ord(c) - ord('a')
+            if node.children[idx] is None:
+                node.children[idx] = Trie()
+            node = node.children[idx]
+        node.v += t
+        node.w = w
+
+    def search(self, pref):
+        node = self
+        for c in pref:
+            idx = 26 if c == ' ' else ord(c) - ord('a')
+            if node.children[idx] is None:
+                return None
+            node = node.children[idx]
+        return node
+
+
+class AutocompleteSystem:
+    def __init__(self, sentences: List[str], times: List[int]):
+        self.trie = Trie()
+        for a, b in zip(sentences, times):
+            self.trie.insert(a, b)
+        self.t = []
+
+    def input(self, c: str) -> List[str]:
+        def dfs(node):
+            if node is None:
+                return
+            if node.v:
+                res.append((node.v, node.w))
+            for nxt in node.children:
+                dfs(nxt)
+
+        if c == '#':
+            s = ''.join(self.t)
+            self.trie.insert(s, 1)
+            self.t = []
+            return []
+
+        res = []
+        self.t.append(c)
+        node = self.trie.search(''.join(self.t))
+        if node is None:
+            return res
+        dfs(node)
+        res.sort(key=lambda x: (-x[0], x[1]))
+        return [v[1] for v in res[:3]]
+obj = AutocompleteSystem(["i love you", "island", "iroman", "i love leetcode"], [2, 2, 5, 3])
+print(obj.input(""))
+print(obj.input("a"))
+print(obj.input("a"))
+print(obj.input("#"))
+
+obj = AutocompleteSystem(["i love you", "island", "iroman", "i love leetcode"], [5,3,2,2])
+print(obj.input("i"))
+print(obj.input(""))
+print(obj.input("a"))
+print(obj.input("#"))
+
